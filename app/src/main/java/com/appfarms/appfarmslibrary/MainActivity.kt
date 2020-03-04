b76892ba9385ae7a9d67feb61d370f54a9635a41
Timon Knispel <t.knispel@appfarms.com>
2020-03-04 12:40:51 +0100

    migrated to androidx

--- app/src/main/java/com/appfarms/appfarmslibrary/MainActivity.kt
+++ app/src/main/java/com/appfarms/appfarmslibrary/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.appfarms.appfarmslibrary
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.appfarms.afsigdialog.AfSigDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
